57ea3ad52093283d719ac3c490c4d403cae5c8a1
Brin29 <breinerstivenparracortes50@gmail.com>
2025-06-09 09:14:49 -0500

    cambios

---
+++ src/components/VerifyEmail.vue
@@ -0,0 +1,17 @@
+<template>
+  <form @submit.prevent="sendOtp">
+    <input type="text">
+    <button type="submit">Enviar</button>
+  </form>
+</template>
+
+<script setup>
+  // import axios from 'axios';
+
+  const VITE_API_URL = import.meta.env.VITE_API_URL
+  
+  const sendOtp = async () => {
+    console.log(VITE_API_URL)
+  }
+  
+</script>
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import UserProfile from "./components/UserProfile.vue";
 import HomeComponent from "./components/HomeComponent.vue";
 import GeneratePetsName from "./components/GeneratePetsName.vue";
 import ResponsePetsName from "./components/ResponsePetsName.vue";
+import VerifyEmail from "./components/VerifyEmail.vue";
 
 const router = createRouter({
   history: createWebHistory(),
@@ -38,6 +39,11 @@ const router = createRouter({
       path: '/response-name',
       name: 'ResponseName',
       component: ResponsePetsName
+    },
+    {
+      path: '/verify-email',
+      name: 'VerifyEmail',
+      component: VerifyEmail
     }
   ]
 })
