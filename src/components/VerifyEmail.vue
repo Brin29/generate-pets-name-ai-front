5e33e78cf8868dfdc46cc02372a51b84543b982d
Brin29 <breinerstivenparracortes50@gmail.com>
2025-06-09 16:54:17 -0500

    cambios

--- src/components/VerifyEmail.vue
+++ src/components/VerifyEmail.vue
@@ -1,17 +1,27 @@
+<script setup>
+import axios from 'axios';
+
+// import axios from 'axios';
+    
+const VITE_APP_URL = import.meta.env.VITE_APP_URL
+      
+const sendOtp = async () => {
+  try{
+    const response = await axios.post(`http://localhost:8000/auth/v1/verify-email/`, {otp:'385129'})
+    
+    if (response.status === 200){
+      console.log('Funciona')
+    }
+  }
+  catch (err) {
+    console.error(err)
+  }
+}
+</script>
+
 <template>
   <form @submit.prevent="sendOtp">
     <input type="text">
     <button type="submit">Enviar</button>
   </form>
 </template>
-
-<script setup>
-  // import axios from 'axios';
-
-  const VITE_API_URL = import.meta.env.VITE_API_URL
-  
-  const sendOtp = async () => {
-    console.log(VITE_API_URL)
-  }
-  
-</script>
